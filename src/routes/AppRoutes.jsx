35c433a8bb8f1a55d72851a4693c315b82e57795
Joshua David Bobadilla Diaz <joshuabobadilla31@gmail.com>
2026-05-29 15:57:15 -0500

    Final

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,64 +1,74 @@
-import { Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
 import Inventory from "../pages/Inventory";
 import Products from "../pages/Products";
 import Alerts from "../pages/Alerts";
-import Scan from "../pages/Scan";   // ← AGREGA ESTA LÍNEA
+import Scan from "../pages/Scan";
+import ProtectedRoute
+from "./ProtectedRoute";
 
 export default function AppRoutes() {
+
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
 
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
+    <BrowserRouter>
 
-      {/* ↓ AGREGA ESTE BLOQUE COMPLETO ↓ */}
-      <Route
-        path="/scan"
-        element={
-          <ProtectedRoute>
-            <Scan />
-          </ProtectedRoute>
-        }
-      />
-      {/* ↑ FIN del bloque a agregar ↑ */}
+      <Routes>
 
-      <Route
-        path="/inventory"
-        element={
-          <ProtectedRoute>
-            <Inventory />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/products"
-        element={
-          <ProtectedRoute>
-            <Products />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/inventory"
+          element={
+            <ProtectedRoute>
+              <Inventory />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/alerts"
-        element={
-          <ProtectedRoute>
-            <Alerts />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute>
+              <Products />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/alerts"
+          element={
+            <ProtectedRoute>
+              <Alerts />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        
+        <Route
+          path="/scan"
+          element={
+            <ProtectedRoute>
+              <Scan />
+            </ProtectedRoute>
+          }
+          />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
