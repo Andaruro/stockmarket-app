4ec5bcb41afcc364a5163cefa09db132df8aa573
Joshua David Bobadilla Diaz <joshuabobadilla31@gmail.com>
2026-05-29 15:54:17 -0500

    final

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,73 +1,64 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
-import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
 import Inventory from "../pages/Inventory";
 import Products from "../pages/Products";
 import Alerts from "../pages/Alerts";
-
-import ProtectedRoute
-from "./ProtectedRoute";
+import Scan from "../pages/Scan";   // ← AGREGA ESTA LÍNEA
 
 export default function AppRoutes() {
-
   return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
-    <BrowserRouter>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
 
-      <Routes>
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/inventory"
-          element={
-            <ProtectedRoute>
-              <Inventory />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/products"
-          element={
-            <ProtectedRoute>
-              <Products />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/alerts"
-          element={
-            <ProtectedRoute>
-              <Alerts />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-        <Route
-          path="/scan"
-          element={
+      {/* ↓ AGREGA ESTE BLOQUE COMPLETO ↓ */}
+      <Route
+        path="/scan"
+        element={
           <ProtectedRoute>
             <Scan />
           </ProtectedRoute>
-  }
-/>
+        }
+      />
+      {/* ↑ FIN del bloque a agregar ↑ */}
 
-      </Routes>
+      <Route
+        path="/inventory"
+        element={
+          <ProtectedRoute>
+            <Inventory />
+          </ProtectedRoute>
+        }
+      />
 
-    </BrowserRouter>
+      <Route
+        path="/products"
+        element={
+          <ProtectedRoute>
+            <Products />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/alerts"
+        element={
+          <ProtectedRoute>
+            <Alerts />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
